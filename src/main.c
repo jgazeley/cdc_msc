86bb364541228c1359094d6a0165c8df629238c3
Jayson <jayson.gazeley@live.com>
2025-06-21 15:21:39 -0700

    Basic glitchy REPL and MSC

--- src/main.c
+++ src/main.c
@@ -1,56 +1,27 @@
-/*
- * The MIT License (MIT)
- *
- * Copyright (c) 2019 Ha Thach (tinyusb.org)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
- */
-
 #include "bsp/board_api.h"
 #include "tusb.h"
+#include <string.h>
+#include <stdbool.h>
+#include <ctype.h> // Include for tolower()
+#include "main_state.h" // Include our new header
 
 //--------------------------------------------------------------------+
-// MACRO CONSTANT TYPEDEF PROTYPES
+// Main Application State
 //--------------------------------------------------------------------+
+// This is the one true definition of the global state variable.
+operating_mode_t current_mode = MODE_MSC_WITH_BASIC_REPL;
 
-/* Blink pattern
- * - 250 ms  : device not mounted
- * - 1000 ms : device mounted
- * - 2500 ms : device is suspended
- */
-enum {
-  BLINK_NOT_MOUNTED = 250,
-  BLINK_MOUNTED = 1000,
-  BLINK_SUSPENDED = 2500,
-};
-
-static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
-
-void led_blinking_task(void);
-void cdc_task(void);
+//--------------------------------------------------------------------+
+// REPL and Task Declarations
+//--------------------------------------------------------------------+
+void basic_repl_task(void);
+void full_repl_task(void);
+void str_to_lower(char* str);
 
 /*------------- MAIN -------------*/
 int main(void) {
   board_init();
 
-  // init device stack on configured roothub port
   tusb_rhport_init_t dev_init = {
     .role = TUSB_ROLE_DEVICE,
     .speed = TUSB_SPEED_AUTO
@@ -63,94 +34,116 @@ int main(void) {
 
   while (1) {
     tud_task(); // tinyusb device task
-    led_blinking_task();
 
-    cdc_task();
-  }
-}
-
-//--------------------------------------------------------------------+
-// Device callbacks
-//--------------------------------------------------------------------+
-
-// Invoked when device is mounted
-void tud_mount_cb(void) {
-  blink_interval_ms = BLINK_MOUNTED;
-}
-
-// Invoked when device is unmounted
-void tud_umount_cb(void) {
-  blink_interval_ms = BLINK_NOT_MOUNTED;
-}
-
-// Invoked when usb bus is suspended
-// remote_wakeup_en : if host allow us  to perform remote wakeup
-// Within 7ms, device must draw an average of current less than 2.5 mA from bus
-void tud_suspend_cb(bool remote_wakeup_en) {
-  (void) remote_wakeup_en;
-  blink_interval_ms = BLINK_SUSPENDED;
-}
-
-// Invoked when usb bus is resumed
-void tud_resume_cb(void) {
-  blink_interval_ms = tud_mounted() ? BLINK_MOUNTED : BLINK_NOT_MOUNTED;
-}
-
-
-//--------------------------------------------------------------------+
-// USB CDC
-//--------------------------------------------------------------------+
-void cdc_task(void) {
-  // connected() check for DTR bit
-  // Most but not all terminal client set this when making connection
-  // if ( tud_cdc_connected() )
-  {
-    // connected and there are data available
-    if (tud_cdc_available()) {
-      // read data
-      char buf[64];
-      uint32_t count = tud_cdc_read(buf, sizeof(buf));
-      (void) count;
-
-      // Echo back
-      // Note: Skip echo by commenting out write() and write_flush()
-      // for throughput test e.g
-      //    $ dd if=/dev/zero of=/dev/ttyACM0 count=10000
-      tud_cdc_write(buf, count);
-      tud_cdc_write_flush();
+    // Call the correct task based on the current operating mode
+    switch (current_mode) {
+      case MODE_MSC_WITH_BASIC_REPL:
+        basic_repl_task();
+        break;
+      case MODE_FULL_REPL:
+        full_repl_task();
+        break;
     }
   }
 }
 
-// Invoked when cdc when line state changed e.g connected/disconnected
-void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts) {
-  (void) itf;
-  (void) rts;
-
-  // TODO set some indicator
-  if (dtr) {
-    // Terminal connected
-  } else {
-    // Terminal disconnected
+//--------------------------------------------------------------------+
+// Helper function to convert a string to lowercase
+//--------------------------------------------------------------------+
+void str_to_lower(char* str) {
+  for (int i = 0; str[i]; i++) {
+    // Cast the result of tolower() back to char to satisfy the compiler.
+    // Also cast the input to unsigned char as is standard practice for ctype functions.
+    str[i] = (char) tolower((unsigned char) str[i]);
   }
 }
 
-// Invoked when CDC interface received data from host
-void tud_cdc_rx_cb(uint8_t itf) {
-  (void) itf;
+//--------------------------------------------------------------------+
+// Basic REPL Task (Only listens for the switch-to-REPL command)
+//--------------------------------------------------------------------+
+void basic_repl_task(void) {
+  if (!tud_cdc_available()) return;
+
+  static char command_buffer[64];
+  static uint32_t buffer_pos = 0;
+
+  int32_t c = tud_cdc_read_char();
+  if (c == -1) return;
+
+  if (c == '\r') {
+    command_buffer[buffer_pos] = '\0';
+    str_to_lower(command_buffer); // Convert to lowercase
+
+    if (strcmp(command_buffer, "repl") == 0) { // Compare with lowercase
+      current_mode = MODE_FULL_REPL;
+      board_delay(10);
+      tud_cdc_write_str("\r\n--- Switched to Full REPL Mode ---\r\n");
+      tud_cdc_write_str("Drive is now unmounted.\r\n> ");
+      tud_cdc_write_flush();
+    } else if (buffer_pos > 0) {
+      tud_cdc_write_str("\r\nSend 'repl' to enter maintenance mode.\r\n");
+      tud_cdc_write_flush();
+    }
+    buffer_pos = 0;
+  } else if (c >= ' ' && c <= '~') {
+    if (buffer_pos < sizeof(command_buffer) - 1) {
+      command_buffer[buffer_pos++] = (char) c;
+    }
+  }
 }
 
 //--------------------------------------------------------------------+
-// BLINKING TASK
+// Full REPL Task (The main command-line interface)
 //--------------------------------------------------------------------+
-void led_blinking_task(void) {
-  static uint32_t start_ms = 0;
-  static bool led_state = false;
+void full_repl_task(void) {
+  if (!tud_cdc_available()) return;
 
-  // Blink every interval ms
-  if (board_millis() - start_ms < blink_interval_ms) return; // not enough time
-  start_ms += blink_interval_ms;
+  static char command_buffer[64];
+  static uint32_t buffer_pos = 0;
 
-  board_led_write(led_state);
-  led_state = 1 - led_state; // toggle
+  int32_t c = tud_cdc_read_char();
+  if (c == -1) return;
+
+  if (c == '\r') {
+    tud_cdc_write_str("\r\n");
+    command_buffer[buffer_pos] = '\0';
+    str_to_lower(command_buffer); // Convert to lowercase
+
+    if (strcmp(command_buffer, "mount") == 0) {
+      current_mode = MODE_MSC_WITH_BASIC_REPL;
+      board_delay(10);
+      tud_cdc_write_str("\r\n--- Switched to MSC Mode ---\r\n");
+      tud_cdc_write_str("Drive is now mounted.\r\n");
+      tud_cdc_write_flush();
+    } else if (strcmp(command_buffer, "help") == 0) {
+      tud_cdc_write_str("Full REPL Commands:\r\n");
+      tud_cdc_write_str("  help   - Show this message\r\n");
+      tud_cdc_write_str("  status - Show system status\r\n");
+      tud_cdc_write_str("  mount  - Return to MSC mode\r\n");
+    } else if (strcmp(command_buffer, "status") == 0) {
+      tud_cdc_write_str("System Status: OK\r\n");
+    } else if (buffer_pos > 0) {
+      tud_cdc_write_str("Unknown command in REPL mode.\r\n");
+    }
+
+    buffer_pos = 0;
+    tud_cdc_write_str("> ");
+    tud_cdc_write_flush();
+  } else if (c == '\b' || c == 127) {
+    if (buffer_pos > 0) { buffer_pos--; tud_cdc_write_str("\b \b"); }
+  } else if (c >= ' ' && c <= '~') {
+    if (buffer_pos < sizeof(command_buffer) - 1) {
+      command_buffer[buffer_pos++] = (char)c;
+      tud_cdc_write_char((char)c);
+    }
+  }
+  tud_cdc_write_flush();
 }
+
+//--------------------------------------------------------------------+
+// Core USB Device callbacks
+//--------------------------------------------------------------------+
+void tud_mount_cb(void) {}
+void tud_umount_cb(void) {}
+void tud_suspend_cb(bool remote_wakeup_en) { (void)remote_wakeup_en; }
+void tud_resume_cb(void) {}
